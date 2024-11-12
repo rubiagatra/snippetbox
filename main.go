39a4c0a39b2db7339153e8b7eacb165e05225463
Doni Rubiagatra <doni@zero-one-group.com>
2024-11-12 12:37:19 +0700

    feat: add http server

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippet Box"))
+}
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
 	fmt.Println("Hello Snippet Box")
+
+	log.Println("Starting server on:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
